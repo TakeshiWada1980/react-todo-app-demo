19ea3d89741084d54bae1cf5fc4010f7f38c7b68
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-09-28 19:40:49 +0900

    create 1 file and update 3 files

--- src/Router.tsx
+++ src/Router.tsx
@@ -39,13 +39,27 @@
 
 import {
   Route,
-  createHashRouter,
+  // createHashRouter,
   createRoutesFromElements,
 } from "react-router-dom";
 import App from "./01/App";
 
-const routes = createRoutesFromElements(<Route path="/" element={<App />} />);
+// const routes = createRoutesFromElements(<Route path="/" element={<App />} />);
 
-export const Router = createHashRouter(routes, {
+// export const Router = createHashRouter(routes);
+
+// export const Router = createHashRouter(routes, {
+//   basename: "/react-todo-app-demo",
+// });
+
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createRoutesFromElements(
+  <>
+    <Route path="/react-todo-app-demo" element={<App />} />
+  </>
+);
+
+export const Router = createBrowserRouter(routes, {
   basename: "/react-todo-app-demo",
 });
