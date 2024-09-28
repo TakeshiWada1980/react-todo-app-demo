580c8b80ac7139c55e8c41edcb3cee75d58c206b
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-09-28 20:11:41 +0900

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/react-todo-app-demo/", // リポジトリ名を指定
-  base: process.env.NODE_ENV === "production" ? "/react-todo-app-demo/" : "/",
+  base: "/react-todo-app-demo/", // リポジトリ名を指定
+  // base: process.env.NODE_ENV === "production" ? "/react-todo-app-demo/" : "/",
   build: {
     rollupOptions: {
       input: {
