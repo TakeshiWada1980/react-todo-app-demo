f302d0861cd18c1d7fe24286933106b088288072
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-09-28 18:02:50 +0900

    Update configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/react-todo-app-demo/", // リポジトリ名を指定
   server: {
     port: 3000, // デフォルトのポートを3000に設定
     strictPort: false,
