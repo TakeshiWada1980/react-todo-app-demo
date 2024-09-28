9d9ddb01c0765d42832225ff23069f9d3d232fb8
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-09-28 18:11:37 +0900

    update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -13,6 +13,30 @@
 
 // export const Router = createBrowserRouter(routes);
 
+// import {
+//   Route,
+//   createHashRouter,
+//   createRoutesFromElements,
+// } from "react-router-dom";
+// import App from "./01/App";
+
+// const routes = createRoutesFromElements(
+//   <>
+//     <Route path="/" element={<App />} />
+//   </>
+// );
+
+// const getBasename = () => {
+//   if (process.env.NODE_ENV === "production") {
+//     return "/react-todo-app-demo";
+//   }
+//   return "/";
+// };
+
+// export const Router = createHashRouter(routes, {
+//   basename: getBasename(),
+// });
+
 import {
   Route,
   createHashRouter,
@@ -20,19 +44,8 @@ import {
 } from "react-router-dom";
 import App from "./01/App";
 
-const routes = createRoutesFromElements(
-  <>
-    <Route path="/" element={<App />} />
-  </>
-);
-
-const getBasename = () => {
-  if (process.env.NODE_ENV === "production") {
-    return "/react-todo-app-demo";
-  }
-  return "/";
-};
+const routes = createRoutesFromElements(<Route path="/" element={<App />} />);
 
 export const Router = createHashRouter(routes, {
-  basename: getBasename(),
+  basename: "/react-todo-app-demo",
 });
