56e320543885ec0c8b74f9557add729108b58755
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-09-28 20:10:59 +0900

    delete 1 file, create 1 file and update 3 files

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Router } from "./Router";
 import { RouterProvider } from "react-router";
 
-import App from "./01/App.tsx";
+// import App from "./01/App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-todo-app-demo/", // リポジトリ名を指定
+  // base: "/react-todo-app-demo/", // リポジトリ名を指定
+  base: process.env.NODE_ENV === "production" ? "/react-todo-app-demo/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html"),
+      },
+    },
+  },
   server: {
     port: 3000, // デフォルトのポートを3000に設定
     strictPort: false,
